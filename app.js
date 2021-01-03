9ae477a6c5ddc0d6bc7e4aded2e731e74a9491f8
Jenil Khandhara <jkhandhara@gmail.com>
2021-01-03 22:50:39 +0530

    Verifying Codeforces handle

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // express related
 const express = require("express");
 const app = express();
-
+const bodyParser= require("body-parser");
 // dotenv related
 const dotenv = require("dotenv");
 dotenv.config({path:"./.env",encoding:"utf-8"});
@@ -19,8 +19,9 @@ const server = app.listen(PORT,
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set("views", "./views");
-
 app.use(express.urlencoded({extended:true}) );
+app.use(bodyParser.json());
+
 
 const io = socketio(server,{});
 
@@ -71,6 +72,14 @@ app.get("/:room/user",(req,res,next)=>{
     else { 
         res.render("userForm",{roomName : req.params.room});
     }
+});
+
+app.post("/:room/user",(req,res,next)=>{
+    //console.log(req.body);
+    if(req.body.isuser)
+    res.json({redirect:"/"});
+    else
+    res.json({redirect:"/"+req.params.room+"/user"});
 });
 
 io.on("connection",(socket)=> {
---
+++ public/userScript.js
@@ -0,0 +1,50 @@
+const user_html= document.getElementById('user');
+const roomName= "<%= roomName %>";
+const form= document.querySelector('form');
+console.log("**");
+async function get_user(user)
+{
+    const base_url=`https://codeforces.com/api/user.status?handle=`;
+    const response = await fetch(base_url+user);
+    if(!response.ok)
+    {
+        alert("PLEASE ENTER PROPER USER HANDLE");
+        return;
+    }
+    let data = await response.json();
+    data = data.result;
+    return user;
+}
+
+function user_input(event)
+{
+    get_user(user_html.value)
+        .then((user)=>{
+            const url="/"+roomName+"/user";
+            if(!user)
+            {
+                fetch(url,{
+                    method: "POST",
+                    body: JSON.stringify({user: user, isuser:false})
+                })
+                .then(response => response.json())
+                .then(data => window.location.href = data.redirect)
+                .catch(err => console.log(err));
+            }
+            else
+            {
+                fetch(url,{
+                    method: "POST",
+                    headers: {'Content-Type': 'application/json'},
+                    body: JSON.stringify({user: user, isuser:true})
+                })
+                .then(response => response.json())
+                .then(data => window.location.href = data.redirect)
+                .catch(err => console.log(err));
+            }
+                
+        })
+        .catch(err=> console.log(err))
+    event.preventDefault();
+}
+form.addEventListener("submit",user_input);
